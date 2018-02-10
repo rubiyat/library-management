e606a1c0205abe7f98b09791288a704e1f168f60
Md Hasan Ali <hasan.bubt.40@gmail.com>
2018-02-11 01:34:31 +0600

    Laravel Authentication #4

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/admin-panel', function () {
     return view('admin.home.home');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
