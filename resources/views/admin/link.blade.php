b92133300728b71c390e356bb7f9c050e92bb267
Md Hasan Ali <hasan.bubt.40@gmail.com>
2018-02-23 00:19:41 +0600

    admin design rechecked

--- resources/views/admin/link.blade.php
+++ resources/views/admin/link.blade.php
@@ -12,19 +12,19 @@
     <title>@yield('title')</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{ asset('/') }}/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('/') }}admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="{{ asset('/') }}/admin/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="{{ asset('/') }}admin/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="{{ asset('/') }}/admin/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="{{ asset('/') }}admin/dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="{{ asset('/') }}/admin/vendor/morrisjs/morris.css" rel="stylesheet">
+    <link href="{{ asset('/') }}admin/vendor/morrisjs/morris.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="{{ asset('/') }}/admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/') }}admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,21 +56,21 @@
 <!-- /#wrapper -->
 
 <!-- jQuery -->
-<script src="{{ asset('/') }}/admin/vendor/jquery/jquery.min.js"></script>
+<script src="{{ asset('/') }}admin/vendor/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="{{ asset('/') }}/admin/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{ asset('/') }}admin/vendor/bootstrap/js/bootstrap.min.js"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="{{ asset('/') }}/admin/vendor/metisMenu/metisMenu.min.js"></script>
+<script src="{{ asset('/') }}admin/vendor/metisMenu/metisMenu.min.js"></script>
 
 <!-- Morris Charts JavaScript -->
-<script src="{{ asset('/') }}/admin/vendor/raphael/raphael.min.js"></script>
-<script src="{{ asset('/') }}/admin/vendor/morrisjs/morris.min.js"></script>
-<script src="{{ asset('/') }}/admin/data/morris-data.js"></script>
+<script src="{{ asset('/') }}admin/vendor/raphael/raphael.min.js"></script>
+<script src="{{ asset('/') }}admin/vendor/morrisjs/morris.min.js"></script>
+<script src="{{ asset('/') }}admin/data/morris-data.js"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="{{ asset('/') }}/admin/dist/js/sb-admin-2.js"></script>
+<script src="{{ asset('/') }}admin/dist/js/sb-admin-2.js"></script>
 
 </body>
 
