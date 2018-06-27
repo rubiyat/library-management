72a1299f00d379d8fd8be656cf4f1311f51811cd
fearless-hasan <hasan.bubt.40@gmail.com>
2018-06-27 15:36:35 +0600

    book serials #37

---
+++ app/Http/Controllers/BookSerialController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BookSerial;
+use Illuminate\Http\Request;
+
+class BookSerialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BookSerial  $bookSerial
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookSerial $bookSerial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BookSerial  $bookSerial
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookSerial $bookSerial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BookSerial  $bookSerial
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookSerial $bookSerial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BookSerial  $bookSerial
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookSerial $bookSerial)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_06_27_093246_create_book_serials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookSerialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_serials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id')->length(11)->unsigned();
+            $table->integer('serial_no')->length(11)->unsigned();
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books');            
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_serials');
+    }
+}
