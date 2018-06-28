099863d5747f22878ddae2da4edcd65a303d82d4
Md Hasan Ali <hasan.bubt.40@gmail.com>
2018-06-28 15:06:32 +0600

    Borrow Table #40

---
+++ database/migrations/2018_06_28_090058_create_borrows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBorrowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('member_id')->length(11)->unsigned();
+            $table->date('borrow_date');
+            $table->date('borrow_return_date');
+            $table->timestamps();
+
+            $table->foreign('member_id')->references('id')->on('members');            
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrows');
+    }
+}
